7ea53e058013291162278115e8272d608c7f2ee5
Big-Logic <alfredngwayah16@gmail.com>
2025-01-09 13:09:28 +0000

    Configure global styles

---
+++ src/config/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,74 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+ :root {
+  /* font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #242424;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+ }
+
+
+
+ @media (prefers-color-scheme: light) {
+  /* :root {
+    color: #213547;
+    background-color: #ffffff;
+  }
+  a:hover {
+    color: #747bff;
+  }
+  button {
+    background-color: #f9f9f9;
+  } */
+ }
+  *,*::before, *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body, input {
+        font-size: 1.8rem;
+        line-height: 1.7;
+        font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+        font-weight: 400;
+    }
+
+    
+
+
+    h1, h2, h3, h4, h5, h6 {
+        line-height: 1.2;
+        font-family: "Dosis", sans-serif;
+    }
+
+    button {
+        box-shadow: none;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        font-size: 1.6rem;
+    }
+
+    img {
+        width: 100%;
+        height: auto;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`;
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import GlobalStyle from "../config/GlobalStyle/GlobalStyle";
 
 export default () => {
   return (
     <main>
+      <GlobalStyle />
       <Outlet />
     </main>
   );
